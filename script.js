61f6fdf37744a374db0c9785c5fb164062a121da
Student <student@compass.local>
2025-12-09 12:41:34 +0600

    add show films logic

---
+++ script.js
@@ -0,0 +1,34 @@
+const showFilmBtn = document.getElementById('show-films-btn');
+const addFilmBtn = document.getElementById('add-films-btn');
+const findFilmBtn = document.getElementById('search-films-btn');
+const markFilmBtn = document.getElementById('mark-films-btn');
+const contentBlock = document.getElementById('content');
+
+const movies = [
+    { title: 'Inception', isAvailable: true },
+    { title: 'The Dark Knight', isAvailable: true },
+    { title: 'Avatar', isAvailable: false },
+    { title: 'Titanic', isAvailable: true },
+    { title: 'Avengers', isAvailable: false },
+]
+
+const clearContent = () => {
+    contentBlock.innerHTML = '';
+};
+
+const showFilms = () => {
+    clearContent();
+
+    const moviesList = document.createElement('ul'); 
+    contentBlock.append(moviesList);
+    
+    for (const movie of movies) {
+        console.log(movie.title)
+        const movieItem = document.createElement('li')
+        movieItem.textContent = `${movie.title} - ${movie.isAvailable ? 'Доступен' : 'Не доступен'}`
+        moviesList.append(movieItem);
+    }
+}
+
+
+showFilmsBtn.addEventListener('click', showFilms)
