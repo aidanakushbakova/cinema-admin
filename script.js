f1b1f61fab73a858d1673296dd9c07c0d3bc4d27
Student <student@compass.local>
2025-12-15 17:40:12 +0600

    html and js and css

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const showFilmBtn = document.getElementById('show-films-btn');
-const addFilmBtn = document.getElementById('add-films-btn');
-const findFilmBtn = document.getElementById('search-films-btn');
-const markFilmBtn = document.getElementById('mark-films-btn');
+
+const showFilmsBtn = document.getElementById('show-films-btn');
+const addFilmBtn = document.getElementById('add-film-btn');
+const findFilmBtn = document.getElementById('search-film-btn');
+const markFilmBtn = document.getElementById('mark-film-btn');
 const contentBlock = document.getElementById('content');
 
 const movies = [
-    { title: 'Inception', isAvailable: true },
-    { title: 'The Dark Knight', isAvailable: true },
-    { title: 'Avatar', isAvailable: false },
-    { title: 'Titanic', isAvailable: true },
-    { title: 'Avengers', isAvailable: false },
-]
+  { title: 'Inception', isAvailable: true },
+  { title: 'The Dark Knight', isAvailable: true },
+  { title: 'Avatar', isAvailable: false },
+  { title: 'Titanic', isAvailable: true },
+  { title: 'Avengers: Endgame', isAvailable: false },
+];
 
 const clearContent = () => {
-    contentBlock.innerHTML = '';
+  contentBlock.innerHTML = '';
 };
 
 const showFilms = () => {
-    clearContent();
+  clearContent();
 
-    const moviesList = document.createElement('ul'); 
-    contentBlock.append(moviesList);
-    
-    for (const movie of movies) {
-        console.log(movie.title)
-        const movieItem = document.createElement('li')
-        movieItem.textContent = `${movie.title} - ${movie.isAvailable ? 'Доступен' : 'Не доступен'}`
-        moviesList.append(movieItem);
-    }
+  const moviesList = document.createElement('ul');
+  contentBlock.append(moviesList);
+
+  for (const movie of movies) {
+    const movieItem = document.createElement('li');
+    movieItem.textContent = `${movie.title} - ${movie.isAvailable ? 'Доступен' : 'Не доступен'}`;
+    moviesList.append(movieItem);
+  }
 }
 
 
-showFilmsBtn.addEventListener('click', showFilms)
+showFilmsBtn.addEventListener('click',showFilms)
